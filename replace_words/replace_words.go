14d931f495df8382854d904bd63d692b40115f94
razonyang <razonyang@gmail.com>
2017-07-25 13:22:07 +0800

    Added comment

--- replace_words/replace_words.go
+++ replace_words/replace_words.go
@@ -10,6 +10,7 @@ func replaceWords(dict []string, sentence string) string {
 	for i, word := range words {
 		for _, v := range dict {
 			if len(word) >= len(v) && word[:len(v)] == v {
+				// in order to find the shortest root, doesn't break on here
 				words[i] = v
 			}
 		}
